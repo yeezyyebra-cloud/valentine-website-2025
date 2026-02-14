086ea750d6958a42473a40371ae9dc242ab86309
yeezyyebra-cloud <yeezyyebra@gmail.com>
2026-02-14 17:11:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const CONFIG = {
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        bears: ['🧸', '🐻', '🐱', '🐈']                       // Cute bear emojis
     },
 
     // Questions and answers
